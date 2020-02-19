507f41c86f1d230ab8d1d81124a4fa64fa66a7f2
Mwangi Muriuki <muriukimwangi.mm@gmail.com>
2020-02-19 15:11:31 +0300

    Initial Commit

---
+++ Quizzer/Controller/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Quizzer
+//
+//  Created by ERNEST MURIUKI on 2/19/20.
+//  Copyright © 2020 ERNEST MURIUKI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+
+    @IBAction func answerSelected(_ sender: UIButton) {
+    }
+    
+}
+
